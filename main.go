9d7d994624da9baf21c916d08a752243c2c2a97d
Stepan-Ryzhov <stepan.2006.dip@gmail.com>
2026-03-08 10:33:44 +0300

    добавление html-страниц

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
 )
 
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Привет! Сервер работает")
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, _ := template.ParseFiles("templates/index.html")
+		tmpl.Execute(w, nil)
+	})
 
-    fmt.Println("Сервер запущен на порту " + port)
-    http.ListenAndServe(":"+port, nil)
+	fmt.Println("Сервер запущен на порту " + port)
+	http.ListenAndServe(":"+port, nil)
 }
